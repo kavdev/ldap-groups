2c67807c840d4bb014791c9c9da3bbc87d3da47d
kavanaugh_development <none@none>
2014-10-05 15:04:14 -0700

    pep8 refactoring
    --HG--
    branch : python3-ldap

--- ldap_groups/exceptions.py
+++ ldap_groups/exceptions.py
@@ -19,33 +19,41 @@
 
 """
 
+
 class AccountDoesNotExist(Exception):
     """The requested user does not exist."""
     pass
+
 
 class InvalidGroupDN(Exception):
     """The Group DN provided is invalid."""
     pass
 
+
 class ImproperlyConfigured(Exception):
     """LDAP Groups is somehow improperly configured."""
     pass
+
 
 class InvalidCredentials(ImproperlyConfigured):
     """The bind dn and password provided are invalid."""
     pass
 
+
 class LDAPServerUnreachable(Exception):
     """The LDAP server is unreachable."""
     pass
+
 
 class ModificationFailed(Exception):
     """The AD Group could not be modified."""
     pass
 
+
 class AccountAlreadyExists(ModificationFailed):
     """The account name provided already exists in a group being modified."""
     pass
+
 
 class InsufficientPermissions(ModificationFailed):
     """The bind user does not have permission to modify a group."""
