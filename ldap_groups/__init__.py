6cbb9faa08700f28d6a19cf69a4effd147fa3438
kavanaugh_development <none@none>
2014-10-13 00:51:21 -0700

    version bump
    --HG--
    branch : python3-ldap

--- ldap_groups/__init__.py
+++ ldap_groups/__init__.py
@@ -29,5 +29,5 @@ from .groups import ADGroup
 # Version Classification
 # Major Updates, Minor Updates, Revision/Bugfix Updates
 #
-VERSION = ("3", "0", "2")
+VERSION = ("3", "0", "3")
 __version__ = ".".join(VERSION)
